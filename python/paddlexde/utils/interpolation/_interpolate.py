3375755ed75d3e4b237b24313246d914301953cf
DrownFish19 <DrownFish19@gmail.com>
2023-08-10 02:45:40 +0000

    add CubicSpline

--- python/paddlexde/utils/interpolation/_interpolate.py
+++ python/paddlexde/utils/interpolation/_interpolate.py
@@ -60,3 +60,89 @@ class LinearInterpolation(NDInterpolationBase):
         fractional_part, index = self._interpret_t(t)
         deriv = self._derivs[..., index, :]
         return deriv
+
+
+class CubicSpline(NDInterpolationBase):
+    """Calculates the cubic spline approximation to the batch of controls given. Also calculates its derivative.
+
+    Example:
+        # (2, 1) are batch dimensions. 7 is the time dimension (of the same length as t). 3 is the channel dimension.
+        x = torch.rand(2, 1, 7, 3)
+        coeffs = natural_cubic_coeffs(x)
+        # ...at this point you can save coeffs, put it through PyTorch's Datasets and DataLoaders, etc...
+        spline = CubicSpline(coeffs)
+        point = torch.tensor(0.4)
+        # will be a tensor of shape (2, 1, 3), corresponding to batch and channel dimensions
+        out = spline.derivative(point)
+    """
+
+    def __init__(self, coeffs, t=None, **kwargs):
+        """
+        Arguments:
+            coeffs: As returned by `torchcde.natural_cubic_coeffs`.
+            t: As passed to linear_interpolation_coeffs. (If it was passed. If you are using neural CDEs then you **do
+                not need to use this argument**. See the Further Documentation in README.md.)
+        """
+        super(CubicSpline, self).__init__(**kwargs)
+
+        if t is None:
+            t = paddle.linspace(
+                0,
+                coeffs.size(-2),
+                coeffs.size(-2) + 1,
+                dtype=coeffs.dtype,
+                device=coeffs.device,
+            )
+
+        channels = coeffs.size(-1) // 4
+        if channels * 4 != coeffs.size(-1):  # check that it's a multiple of 4
+            raise ValueError("Passed invalid coeffs.")
+        a, b, two_c, three_d = (
+            coeffs[..., :channels],
+            coeffs[..., channels : 2 * channels],
+            coeffs[..., 2 * channels : 3 * channels],
+            coeffs[..., 3 * channels :],
+        )
+
+        self.register_buffer("_t", t)
+        self.register_buffer("_a", a)
+        self.register_buffer("_b", b)
+        # as we're typically computing derivatives, we store the multiples of these coefficients that are more useful
+        self.register_buffer("_two_c", two_c)
+        self.register_buffer("_three_d", three_d)
+
+    @property
+    def grid_points(self):
+        return self._t
+
+    @property
+    def interval(self):
+        return paddle.stack([self._t[0], self._t[-1]])
+
+    def _interpret_t(self, t):
+        t = paddle.to_tensor(t, dtype=self._b.dtype)
+        maxlen = self._b.size(-2) - 1
+        # clamp because t may go outside of [t[0], t[-1]]; this is fine
+        index = paddle.bucketize(t.detach(), self._t.detach()).sub(1).clamp(0, maxlen)
+        # will never access the last element of self._t; this is correct behaviour
+        fractional_part = t - self._t[index]
+        return fractional_part, index
+
+    def evaluate(self, t):
+        fractional_part, index = self._interpret_t(t)
+        fractional_part = fractional_part.unsqueeze(-1)
+        inner = (
+            0.5 * self._two_c[..., index, :]
+            + self._three_d[..., index, :] * fractional_part / 3
+        )
+        inner = self._b[..., index, :] + inner * fractional_part
+        return self._a[..., index, :] + inner * fractional_part
+
+    def derivative(self, t):
+        fractional_part, index = self._interpret_t(t)
+        fractional_part = fractional_part.unsqueeze(-1)
+        inner = (
+            self._two_c[..., index, :] + self._three_d[..., index, :] * fractional_part
+        )
+        deriv = self._b[..., index, :] + inner * fractional_part
+        return deriv
