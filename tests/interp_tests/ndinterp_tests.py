e7f905b6c37a41f12a8e26e8514131242d76874b
DrowFish19 <DrownFish19@gmail.com>
2023-08-14 14:46:22 +0800

    update

--- tests/interp_tests/ndinterp_tests.py
+++ tests/interp_tests/ndinterp_tests.py
@@ -9,9 +9,13 @@ paddle.device.set_device("cpu")
 @pytest.mark.interp_linear_tests
 def test_linear_interp():
     pX, pY = get_data()
+    # pX [B,T,D]
     X = LinearInterpolation(pX)
-    X0 = X.evaluate(X.interval[0])
-    print(X0)
+    X0 = X.evaluate(X.interval[0])  # [B,D] # 特定时刻的数值
+    X0_der = X.derivative(X.interval[0])  # [B,1,D] 特定时刻的导数
+
+    print("X0 shape:", X0.shape)
+    print("X0_der shape", X0_der.shape)
     # assert paddle.allclose(sol, y, rtol=4e-3)
 
 
