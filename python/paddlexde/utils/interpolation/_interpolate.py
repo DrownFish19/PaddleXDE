162c9e7640691f6e07f32f56d5ceb179b61d3c37
DrownFish19 <DrownFish19@gmail.com>
2023-08-14 10:02:48 +0800

    update CubicHermiteSpline

--- python/paddlexde/utils/interpolation/_interpolate.py
+++ python/paddlexde/utils/interpolation/_interpolate.py
@@ -6,10 +6,10 @@ from .interpnd import NDInterpolationBase
 class LinearInterpolation(NDInterpolationBase):
     """Calculates the linear interpolation to the batch of controls given. Also calculates its derivative."""
 
-    def __init__(self, coeffs, t=None, **kwargs):
+    def __init__(self, series, t=None, **kwargs):
         """
         Arguments:
-            coeffs: As returned by linear_interpolation_coeffs.
+            series: As returned by linear_interpolation_coeffs.
             t: As passed to linear_interpolation_coeffs. (If it was passed. If you are using neural CDEs then you **do
                 not need to use this argument**. See the Further Documentation in README.md.)
         """
@@ -17,15 +17,15 @@ class LinearInterpolation(NDInterpolationBase):
 
         if t is None:
             t = paddle.linspace(
-                0, coeffs.shape[-2] - 1, coeffs.shape[-2], dtype=coeffs.dtype
+                0, series.shape[-2] - 1, series.shape[-2], dtype=series.dtype
             )
 
-        derivs = (coeffs[..., 1:, :] - coeffs[..., :-1, :]) / (
+        derivs = (series[..., 1:, :] - series[..., :-1, :]) / (
             t[1:] - t[:-1]
         ).unsqueeze(-1)
 
         self.register_buffer("_t", t)
-        self.register_buffer("_coeffs", coeffs)
+        self.register_buffer("_coeffs", series)
         self.register_buffer("_derivs", derivs)
 
     @property
@@ -62,87 +62,81 @@ class LinearInterpolation(NDInterpolationBase):
         deriv = self._derivs[..., index, :]
         return deriv
 
+class CubicHermiteSpline(NDInterpolationBase):
+    
+    def __init__(self, series, t=None, **kwargs):
+        """_summary_
 
-class CubicSpline(NDInterpolationBase):
-    """Calculates the cubic spline approximation to the batch of controls given. Also calculates its derivative.
-
-    Example:
-        # (2, 1) are batch dimensions. 7 is the time dimension (of the same length as t). 3 is the channel dimension.
-        x = torch.rand(2, 1, 7, 3)
-        coeffs = natural_cubic_coeffs(x)
-        # ...at this point you can save coeffs, put it through PyTorch's Datasets and DataLoaders, etc...
-        spline = CubicSpline(coeffs)
-        point = torch.tensor(0.4)
-        # will be a tensor of shape (2, 1, 3), corresponding to batch and channel dimensions
-        out = spline.derivative(point)
-    """
-
-    def __init__(self, coeffs, t=None, **kwargs):
+        Args:
+            series (tensor): B T D
+            t (_type_, optional): _description_. Defaults to None.
         """
-        Arguments:
-            coeffs: As returned by `torchcde.natural_cubic_coeffs`.
-            t: As passed to linear_interpolation_coeffs. (If it was passed. If you are using neural CDEs then you **do
-                not need to use this argument**. See the Further Documentation in README.md.)
-        """
-        super(CubicSpline, self).__init__(**kwargs)
+        super(CubicHermiteSpline).__init__()
 
         if t is None:
             t = paddle.linspace(
                 0,
-                coeffs.shape[-2],
-                coeffs.shape[-2] + 1,
-                dtype=coeffs.dtype,
+                series.shape[-2],
+                series.shape[-2] + 1,
+                dtype=series.dtype,
             )
 
-        channels = coeffs.shape[-1] // 4
-        if channels * 4 != coeffs.shape[-1]:  # check that it's a multiple of 4
-            raise ValueError("Passed invalid coeffs.")
-        a, b, two_c, three_d = (
-            coeffs[..., :channels],
-            coeffs[..., channels : 2 * channels],
-            coeffs[..., 2 * channels : 3 * channels],
-            coeffs[..., 3 * channels :],
-        )
+        # build cubic hemite spline matrix H
+        indices = [[0, 0, 0, 0, 1, 1, 1, 1, 2, 3], [0, 1, 2, 3, 0, 1, 2, 3, 2, 0]]
+        values = [2.0, -2.0, 1.0, 1.0, -3.0, 3.0, -2.0, -1.0, 1.0, 1]
+        dense_shape = [4, 4]
+        h = paddle.sparse.sparse_coo_tensor(indices, values, dense_shape)
 
+        # build cubic hermite spline P
+        derivs = (series[..., 1:, :] - series[..., :-1, :]) / (t[1:] - t[:-1])  # [B, T-1, D]
+        derivs = paddle.concat([derivs, derivs[..., -1:, :]], axis=-2)  # [B T D] # 最后一个点的梯度采用上一个点的梯度
+        
         self.register_buffer("_t", t)
-        self.register_buffer("_a", a)
-        self.register_buffer("_b", b)
-        # as we're typically computing derivatives, we store the multiples of these coefficients that are more useful
-        self.register_buffer("_two_c", two_c)
-        self.register_buffer("_three_d", three_d)
+        self.register_buffer("_h", h)
+        self.register_buffer("_series", series)
+        self.register_buffer("_derivs", derivs)
 
-    @property
-    def grid_points(self):
-        return self._t
 
-    @property
-    def interval(self):
-        return paddle.stack([self._t[0], self._t[-1]])
-
-    def _interpret_t(self, t):
-        t = paddle.to_tensor(t, dtype=self._b.dtype)
-        maxlen = self._b.shape[-2] - 1
+    def Ts(self,t, der=False):
+        # 判断是否是微分过程
+        if not der:
+            ts = [t**3,t**2,t,1]
+        else :
+            ts = [3*t**2,2*t,1,0]
+        
+        return paddle.to_tensor(ts)
+    
+    
+    def interpolate(self, t):
+        t = paddle.to_tensor(t, dtype=self._derivs.dtype)
+        maxlen = self._derivs.shape[-2] - 1
         # clamp because t may go outside of [t[0], t[-1]]; this is fine
-        index = paddle.bucketize(t.detach(), self._t.detach()).sub(1).clamp(0, maxlen)
+        index = (paddle.bucketize(t.detach(), self._t.detach()) - 1).clip(0, maxlen)
         # will never access the last element of self._t; this is correct behaviour
-        fractional_part = t - self._t[index]
+        fractional_part = t - self._t[index : index + 1]
         return fractional_part, index
 
     def evaluate(self, t):
-        fractional_part, index = self._interpret_t(t)
+        fractional_part, index = self.interpolate(t)
         fractional_part = fractional_part.unsqueeze(-1)
-        inner = (
-            0.5 * self._two_c[..., index, :]
-            + self._three_d[..., index, :] * fractional_part / 3
-        )
-        inner = self._b[..., index, :] + inner * fractional_part
-        return self._a[..., index, :] + inner * fractional_part
+        prev_coeff = self._coeffs[..., index : index + 1, :].squeeze(-2)
+        next_coeff = self._coeffs[..., index + 1 : index + 2, :].squeeze(-2)
+        prev_t = self._t[index : index + 1]
+        next_t = self._t[index + 1 : index + 2]
+        diff_t = next_t - prev_t
+        return prev_coeff + fractional_part * (
+            next_coeff - prev_coeff
+        ) / diff_t.unsqueeze(-1)
 
     def derivative(self, t):
-        fractional_part, index = self._interpret_t(t)
-        fractional_part = fractional_part.unsqueeze(-1)
-        inner = (
-            self._two_c[..., index, :] + self._three_d[..., index, :] * fractional_part
-        )
-        deriv = self._b[..., index, :] + inner * fractional_part
+        fractional_part, index = self.interpolate(t)
+        deriv = self._derivs[..., index, :]
         return deriv
+    
+    
+    
+chp = CubicHermiteSpline()
+
+print(chp.h.to_dense())
+        
+        
