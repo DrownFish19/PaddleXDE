093cb356c67320feebb3793ece829d81a7729dae
DrownFish19 <DrownFish19@gmail.com>
2023-09-05 21:32:33 +0800

    temp commit

--- paddlexde/xde/base_xde.py
+++ paddlexde/xde/base_xde.py
@@ -23,7 +23,9 @@ class BaseXDE(ABC, nn.Layer):
         super(BaseXDE, self).__init__()
         self.name = name
         self.var_nums = var_nums  # 返回值数量
-
+        # y0输入后全部进行flatten操作，进行展平
+        # 后续计算过程中，如果需要调用func操作，可以通过xde进行输入，在xde中进行unflatten操作，然后将数据传给func
+        # 当全部数据计算结束后，进行unflatten操作，需要注意sol已经增加多个，需要根据数据形状进行展开
         if isinstance(y0, tuple):
             self.is_tuple = True
             self.shapes = [y0_.shape for y0_ in y0]
@@ -89,3 +91,27 @@ class BaseXDE(ABC, nn.Layer):
     def method(self):
         print(f"current method is {self.name}.")
         return self.name
+    
+    def unflatten(self, tensor, length, shapes, numels):
+        tensor_list = []
+        total = 0
+
+        for shape, num_ele in zip(shapes, numels):
+            next_total = total + num_ele
+            # It's important that this be view((...)), not view(...). Else when length=(), shape=() it fails.
+            if len(shape) == 0:
+                tensor_list.append(tensor[..., total:next_total].reshape((*length, 0)))
+            else:
+                tensor_list.append(tensor[..., total:next_total].reshape((*length, *shape)))
+            total = next_total
+        return tuple(tensor_list)
+    
+    
+    def flatten(self):
+        self.shapes = [y0_.shape for y0_ in y0]
+        self.num_elements = [paddle.numel(y0_) for y0_ in y0]
+        self.y0 = paddle.concat([y0_.reshape([-1]) for y0_ in y0])
+        
+    def call_dunc(self, **kwargs):
+        pass
+
