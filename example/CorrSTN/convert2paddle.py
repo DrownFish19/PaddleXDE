1228932d085f9129ab5f545aa1a8425659724d88
DrownFish19 <DrownFish19@gmail.com>
2023-10-12 11:10:56 +0000

    update convert

--- example/CorrSTN/convert2paddle.py
+++ example/CorrSTN/convert2paddle.py
@@ -1,3 +1,6 @@
+encoder_layer_num = 4
+decoder_layer_num = 4
+
 mapping_dict = {
     "src_embed.0.weight": "encoder_dense.weight",
     "src_embed.0.bias": "encoder_dense.bias",
@@ -13,13 +16,37 @@ mapping_dict = {
     "trg_embed.2.gcn_smooth_layers.0.Theta.weight": "decode_spatial_position.gcn_smooth_layers.0.Theta.weight",
     "trg_embed.2.gcn_smooth_layers.0.alpha": "decode_spatial_position.gcn_smooth_layers.0.alpha",  # TODO
     "trg_embed.2.gcn_smooth_layers.0.beta": "decode_spatial_position.gcn_smooth_layers.0.beta",  # TODO
-    "*.self_attn.conv1Ds_aware_temporal_context.0.weight": "*.self_attn.query_conv.weight",
-    "*.self_attn.conv1Ds_aware_temporal_context.0.bias": "*.self_attn.query_conv.bias ",
-    "*.self_attn.conv1Ds_aware_temporal_context.1.weight": "*.self_attn.key_conv.weight",
-    "*.self_attn.conv1Ds_aware_temporal_context.1.bias": "*.self_attn.key_conv.bias",
-    "*.feed_forward_gcn.gcn.alpha": "*.feed_forward_gcn.gcn.alpha",  # TODO
-    "*.feed_forward_gcn.gcn.beta": "*.feed_forward_gcn.gcn.beta",  # TODO
-    "*.feed_forward_gcn.gcn.Theta.weight": "*.feed_forward_gcn.linear.weight",
     "prediction_generator.weight": "generator.weight",
     "prediction_generator.bias": "generator.bias",
 }
+
+en_decoder_mapping_dict = {
+    ".self_attn.conv1Ds_aware_temporal_context.0.weight": ".self_attn.query_conv.weight",
+    ".self_attn.conv1Ds_aware_temporal_context.0.bias": ".self_attn.query_conv.bias ",
+    ".self_attn.conv1Ds_aware_temporal_context.1.weight": ".self_attn.key_conv.weight",
+    ".self_attn.conv1Ds_aware_temporal_context.1.bias": ".self_attn.key_conv.bias",
+    ".feed_forward_gcn.gcn.alpha": ".feed_forward_gcn.alpha",
+    ".feed_forward_gcn.gcn.beta": ".feed_forward_gcn.beta",
+    ".feed_forward_gcn.gcn.Theta.weight": ".feed_forward_gcn.linear.weight",
+    ".sublayer.0.norm.weight": ".sublayer.0.norm.weight",
+    ".sublayer.0.norm.bias": ".sublayer.0.norm.bias",
+    ".sublayer.1.norm.weight": ".sublayer.1.norm.weight",
+    ".sublayer.1.norm.bias": ".sublayer.1.norm.bias",
+    ".sublayer.2.norm.weight": ".sublayer.2.norm.weight",
+    ".sublayer.2.norm.bias": ".sublayer.2.norm.bias",
+}
+
+for i in range(encoder_layer_num):
+    for item in en_decoder_mapping_dict:
+        mapping_dict[f"encoder.layers.{i}" + item] = (
+            f"encoder.layers.{i}" + en_decoder_mapping_dict[item]
+        )
+
+for i in range(decoder_layer_num):
+    for item in en_decoder_mapping_dict:
+        mapping_dict[f"decoder.layers.{i}" + item] = (
+            f"decoder.layers.{i}" + en_decoder_mapping_dict[item]
+        )
+
+for key in mapping_dict:
+    print(key, mapping_dict[key])
