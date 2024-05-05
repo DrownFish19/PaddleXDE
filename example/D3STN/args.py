54516eb81dc1dcabc2a0f7fe705325af86dadd74
DrownFish19 <DrownFish19@gmail.com>
2024-05-05 11:31:48 +0000

    upload trained models

--- example/D3STN/args.py
+++ example/D3STN/args.py
@@ -11,24 +11,17 @@ parser.add_argument(
     "--dataset_name", type=str, default="HZME_OUTFLOW", help="dataset name"
 )
 parser.add_argument(
-    "--data_path",
-    type=str,
-    default="TrafficFlowData/HZME_OUTFLOW/HZME_OUTFLOW.npz",
+    "--data_path", type=str, default="TrafficFlowData/HZME_OUTFLOW/HZME_OUTFLOW.npz"
 )
 parser.add_argument(
-    "--adj_path",
-    type=str,
-    default="TrafficFlowData/HZME_OUTFLOW/HZME_OUTFLOW.csv",
+    "--adj_path", type=str, default="TrafficFlowData/HZME_OUTFLOW/HZME_OUTFLOW.csv"
 )
 parser.add_argument(
-    "--sc_path",
-    type=str,
-    default="TrafficFlowData/HZME_OUTFLOW/SCORR_HZME_OUTFLOW.npy",
+    "--sc_path", type=str, default="TrafficFlowData/HZME_OUTFLOW/SCORR_HZME_OUTFLOW.npy"
 )
 parser.add_argument("--split", type=str, default="6:2:2", help="data split")
 parser.add_argument("--scale", type=bool, default=True, help="data norm scale")
 parser.add_argument("--num_nodes", type=int, default=80)
-
 
 # model config
 parser.add_argument("--model_name", type=str, default="D3STN", help="model name")
@@ -43,16 +36,12 @@ parser.add_argument("--d_model", type=int, default=64, help="d_proj+d_sect*2")
 parser.add_argument("--d_proj", type=int, default=32)
 parser.add_argument("--d_sect", type=int, default=16)
 parser.add_argument("--attention", type=str, default="Corr", help="Corr,Vanilla")
-parser.add_argument(
-    "--split_seq", type=bool, default=False, help="split q k v along seqlen(t=12)"
-)
 parser.add_argument("--head", type=int, default=8, help="head")
 parser.add_argument("--kernel_size", type=int, default=3, help="kernel_size")
 parser.add_argument("--top_k", type=int, default=5, help="top_k")
 parser.add_argument("--smooth_layer_num", type=int, default=1)
 parser.add_argument("--no_adj", type=bool, default=False, help="no adj")
 parser.add_argument("--solver", type=str, default="euler", help="euler,midpoint,rk4")
-
 
 # train config
 parser.add_argument("--learning_rate", type=float, default=1e-3)
